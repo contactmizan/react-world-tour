778b8076c3bd31bc88d1fc3b7f3080d8450a5ff8
contactmizan <contactmizan1206@gmail.com>
2024-03-13 17:15:12 +0600

    added individual country div

--- src/components/Country/Country.jsx
+++ src/components/Country/Country.jsx
@@ -1,14 +1,29 @@
+import { useState } from 'react';
 import './Country.css'
 const Country = ({ country }) => {
     //destructuring
-    const { name, flags, population, area } = country;
+    const { name, flags, population, area, cca3 } = country;
+    //state
+    const [visited, setVisited] = useState(false);
+
+    const handleVisited = () => {
+        setVisited(!visited); //maan change er jonno ! 
+    }
     return (
-        <div className="country">
-            <h3>{name.common}</h3>
+        //conditional css add: visited color cng er jnno
+        <div className={`country ${visited && 'visited'}`}>
+            {/* ternery style */}
+            <h3 style={{ color: visited ? 'purple' : 'whote' }}>{name.common}</h3>
             <img src={flags.png} alt="" />
             <p>Population: {population}</p>
             <p>Area: {area}</p>
-        </div>
+            <p><small>Code: {cca3}</small></p>
+            <button onClick={handleVisited}>{visited ? 'Visited' : 'Going'}</button>
+            {/* {visited && 'I have visited this country.'} */}
+            {/* ternery */}
+            {visited ? 'I have visited.' : 'I want to visit.'}
+
+        </div >
     );
 }
 
