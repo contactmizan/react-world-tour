20bb9bc8b3f801696a70f91eff27ac4f683e3288
contactmizan <contactmizan1206@gmail.com>
2024-03-13 22:08:45 +0600

    now visited country & flag will show the display in a ul

--- src/components/Country/Country.jsx
+++ src/components/Country/Country.jsx
@@ -1,23 +1,33 @@
 import { useState } from 'react';
 import './Country.css'
-const Country = ({ country }) => {
+//ager tay jeita jeita props kore pathaichi eikhane destructure korte hobe function er property hishebe.
+
+const Country = ({ country, handleVisitedCountry, handleVisitedFlags }) => {
     //destructuring
     const { name, flags, population, area, cca3 } = country;
     //state
     const [visited, setVisited] = useState(false);
-
+    //for event handler 
     const handleVisited = () => {
         setVisited(!visited); //maan change er jonno ! 
     }
+
+    const passWithParams = () => handleVisitedCountry(country);
+
+
     return (
         //conditional css add: visited color cng er jnno
         <div className={`country ${visited && 'visited'}`}>
             {/* ternery style */}
-            <h3 style={{ color: visited ? 'purple' : 'whote' }}>{name.common}</h3>
+            <h3 style={{ color: visited ? 'purple' : 'white' }}>{name.common}</h3>
             <img src={flags.png} alt="" />
             <p>Population: {population}</p>
             <p>Area: {area}</p>
             <p><small>Code: {cca3}</small></p>
+            <button onClick={passWithParams}>Mark Visited</button>
+            <br />
+            <button onClick={() => handleVisitedFlags(country.flags.png)}>Add Flag</button>
+            <br />
             <button onClick={handleVisited}>{visited ? 'Visited' : 'Going'}</button>
             {/* {visited && 'I have visited this country.'} */}
             {/* ternery */}
